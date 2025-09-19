6e6179030f56ac167bacbf7e8a7ece2f3c41f213
Gopi vinay <139052698+fake-smiler-115@users.noreply.github.com>
2025-09-19 13:13:06 +0530

    To check the number is armstrong or not

---
+++ jsPrograms/check_armstrong.js
@@ -0,0 +1,42 @@
+const checkNumber = 371 ;
+
+let holdingNumber ;
+
+let convertedNumber = 0 ;
+holdingNumber = checkNumber;
+while(holdingNumber){
+    let lastDigit = holdingNumber % 10; 
+    convertedNumber = convertedNumber + lastDigit * lastDigit * lastDigit ;
+    holdingNumber = holdingNumber - lastDigit ;
+    holdingNumber = holdingNumber / 10 ;
+}
+
+let isArmstrong  ;
+isArmstrong = (convertedNumber === checkNumber) ? true : false ;
+
+
+let finalValue ;
+finalValue = isArmstrong ? "is a armstrong number" : "is not a armstrong number";
+console.log( checkNumber , finalValue);
+
+
+// while(holdNumber){
+//     remainder = holdNumber % 10;
+//     holdNumber = holdNumber - remainder ;
+//     holdNumber = holdNumber / 10 ;
+//     powerToUse = powerToUse + 1;
+// }
+
+
+
+
+
+
+//console.log(powerToUse);
+
+// if(isPalindrome){
+//     console.log( checkNumber , "is a palindrome ");
+// } else {
+//     console.log(checkNumber , "is not a palindrone ");
+// }
+
