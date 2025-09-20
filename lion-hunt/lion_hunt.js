aec26c0db9db3813b368821e00be908da264b869
Gopi vinay <139052698+fake-smiler-115@users.noreply.github.com>
2025-09-20 17:55:29 +0530

    verifing the presences of zebra , lions in the testcase

--- lion-hunt/lion_hunt.js
+++ lion-hunt/lion_hunt.js
@@ -1,3 +1,16 @@
-const minimumSpaces = 5;
+const testCase1 = " Z";
 
-console.log(minimumSpaces);
+let checkLion ;
+checkLion = !(testCase1[0] === 'L'  || testCase1[1]  === 'L' || testCase1[2] === 'L'  || testCase1[3] === 'L'  || testCase1[4] === 'L'  || testCase1[5] === 'L' || testCase1[6]  === 'L' );
+console.log(checkLion);
+
+let checkZebra ;
+checkZebra = !(testCase1[0] === 'Z'  || testCase1[1]  === 'Z' || testCase1[2] === 'Z'  || testCase1[3] === 'Z'  || testCase1[4] === 'Z'  || testCase1[5] === 'Z' || testCase1[6]  === 'Z' );
+console.log(checkZebra);
+
+if(checkLion || checkZebra){
+    console.log(-1);
+    
+}
+
+
