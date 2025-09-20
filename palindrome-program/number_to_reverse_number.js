d1d8bb382df212459c4df2fa89c87407c3cbed08
Gopi vinay <139052698+fake-smiler-115@users.noreply.github.com>
2025-09-20 12:49:19 +0530

    Extracting the last digit and divide by 10 until the number becomes zero

--- palindrome-program/number_to_reverse_number.js
+++ palindrome-program/number_to_reverse_number.js
@@ -1,16 +1,31 @@
 const input = 345;
 let lastDigit ; 
 
-lastDigit = input % 10;
- 
-console.log(lastDigit);
-
 let duplicateInput = input ;
 
-duplicateInput = duplicateInput - lastDigit ;  // else getting floating values in dupicateInput
-
+lastDigit = duplicateInput % 10;
+console.log(lastDigit);
+duplicateInput = duplicateInput - lastDigit ; 
 duplicateInput = duplicateInput / 10 ;
-
-const reverseOfNumber = lastDigit ;
+let reverseOfNumber = lastDigit ;
 
 console.log(duplicateInput);
+
+
+lastDigit = duplicateInput % 10;
+console.log(lastDigit);
+ duplicateInput = duplicateInput - lastDigit ; 
+duplicateInput = duplicateInput / 10 ;
+ reverseOfNumber = lastDigit ;
+
+console.log(duplicateInput);
+
+lastDigit = duplicateInput % 10;
+console.log(lastDigit);
+ duplicateInput = duplicateInput - lastDigit ; 
+duplicateInput = duplicateInput / 10 ;
+ reverseOfNumber = lastDigit ;
+
+console.log(duplicateInput);
+
+ 
