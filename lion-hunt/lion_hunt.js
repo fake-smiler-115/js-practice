4421b6f8078584c3869b71627fba185089b542e7
Gopi vinay <139052698+fake-smiler-115@users.noreply.github.com>
2025-09-20 20:30:08 +0530

    merged space_between_lion_zebra.js 's code to this file

--- lion-hunt/lion_hunt.js
+++ lion-hunt/lion_hunt.js
@@ -1,4 +1,4 @@
-const testCase1 = "L  Z";
+const testCase1 = "Z   Z";
 
 const testCaseLength = testCase1.length ;
 
@@ -20,4 +20,48 @@ for(let indexValue = 0 ; indexValue < testCaseLength ; indexValue++){
 if(checkLion || checkZebra){
     console.log(-1);
    
-} 
+} else{
+    
+    
+    let spaceCount = 0;
+    let minimumSpace ;
+    let isCheck = false ;
+    let startCharacter;
+    
+    for( let indexValue = 0 ; indexValue < testCaseLength ; indexValue++){
+    
+        if(isCheck === false){
+
+            if (testCase1[indexValue] === 'L' || testCase1[indexValue] === 'Z'){
+               spaceCount = 0;
+               isCheck = true;
+               startCharacter = testCase1[indexValue];
+               minimumSpace = 1000; 
+           }
+    
+        }
+         
+        if(isCheck){
+    
+            if(testCase1[indexValue] === ' '){
+                spaceCount = spaceCount + 1;
+               
+            } else if(testCase1[indexValue] === startCharacter){
+                spaceCount = 0;
+
+            }else {
+
+                if(minimumSpace > spaceCount){
+                    minimumSpace = spaceCount;
+                }
+
+                startCharacter = testCase1[indexValue];
+                spaceCount = 0;
+            }
+        }
+        
+        
+    }
+    
+    console.log(minimumSpace);
+}
