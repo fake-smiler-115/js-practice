93da7ef8c686027cd9c15da6b82b611e89670a39
Gopi vinay <139052698+fake-smiler-115@users.noreply.github.com>
2025-09-20 20:23:19 +0530

    arranged  if statements , ischeck block

--- lion-hunt/space_between_lion_zebra.js
+++ lion-hunt/space_between_lion_zebra.js
@@ -1,29 +1,40 @@
-const testCase1 = "L     Z L  Z";
+const testCase1 = " ZL  Z L";
 
 let spaceCount = 0;
-let minimumSpace = 0 ;
+let minimumSpace ;
 const testCaseLength = testCase1.length;
+let isCheck = false ;
+let startCharacter;
 
 for( let indexValue = 0 ; indexValue < testCaseLength ; indexValue++){
 
-    if (testCase1[indexValue] === 'L'){
-        spaceCount = 0;
+    if(isCheck === false){
+        if (testCase1[indexValue] === 'L' || testCase1[indexValue] === 'Z'){
+           spaceCount = 0;
+           isCheck = true;
+           startCharacter = testCase1[indexValue];
+           minimumSpace = 1000; 
+       }
+
     }
+     
+    if(isCheck){
 
-    if(testCase1[indexValue] === ' '){
-        spaceCount = spaceCount + 1;
-       
-    } 
-
-    if (testCase1[indexValue] === 'Z'){
-
-        if(minimumSpace === 0){
-            minimumSpace = spaceCount;
-        }
-        if(minimumSpace > spaceCount){
-            minimumSpace = spaceCount
+        if(testCase1[indexValue] === ' '){
+            spaceCount = spaceCount + 1;
+           
+        } else if(testCase1[indexValue] === startCharacter){
+            spaceCount = 0;
+        }else {
+            if(minimumSpace > spaceCount){
+                minimumSpace = spaceCount;
+            }
+            startCharacter = testCase1[indexValue];
+            spaceCount = 0;
         }
     }
+    
+    
 }
 
 console.log(minimumSpace);
