ae0a00f00dfa080dd1c473540003f1b63edfc909
Gopi vinay <139052698+fake-smiler-115@users.noreply.github.com>
2025-09-20 19:34:14 +0530

    added  if statements

--- lion-hunt/space_between_lion_zebra.js
+++ lion-hunt/space_between_lion_zebra.js
@@ -1,11 +1,11 @@
-const testCase1 = "L  L  Z";
+const testCase1 = "L     Z L  Z";
 
 let spaceCount = 0;
-
+let minimumSpace = 0 ;
 const testCaseLength = testCase1.length;
 
 for( let indexValue = 0 ; indexValue < testCaseLength ; indexValue++){
-    
+
     if (testCase1[indexValue] === 'L'){
         spaceCount = 0;
     }
@@ -14,6 +14,16 @@ for( let indexValue = 0 ; indexValue < testCaseLength ; indexValue++){
         spaceCount = spaceCount + 1;
        
     } 
+
+    if (testCase1[indexValue] === 'Z'){
+
+        if(minimumSpace === 0){
+            minimumSpace = spaceCount;
+        }
+        if(minimumSpace > spaceCount){
+            minimumSpace = spaceCount
+        }
+    }
 }
 
-console.log(spaceCount);
+console.log(minimumSpace);
