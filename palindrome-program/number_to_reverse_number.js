6631313c28f7d3c5a51cc2271ca84d63f319bbc3
Gopi vinay <139052698+fake-smiler-115@users.noreply.github.com>
2025-09-20 13:11:25 +0530

    Added if-condition to check whether the input is zero

--- palindrome-program/number_to_reverse_number.js
+++ palindrome-program/number_to_reverse_number.js
@@ -3,30 +3,52 @@ let lastDigit ;
 
 let duplicateInput = input ;
 
-lastDigit = duplicateInput % 10;
-console.log(lastDigit);
-
-duplicateInput = duplicateInput - lastDigit ; 
-duplicateInput = duplicateInput / 10 ;
 let reverseOfNumber = 0;
-reverseOfNumber = (reverseOfNumber * 10) + lastDigit ;
+if(duplicateInput){
+
+    lastDigit = duplicateInput % 10;
+    console.log(lastDigit);
+
+    duplicateInput = duplicateInput - lastDigit ; 
+    duplicateInput = duplicateInput / 10 ;
+    reverseOfNumber = (reverseOfNumber * 10) + lastDigit ;
+
+}
 
 
-lastDigit = duplicateInput % 10;
-console.log(lastDigit);
+if(duplicateInput){
 
-duplicateInput = duplicateInput - lastDigit ; 
-duplicateInput = duplicateInput / 10 ;
-reverseOfNumber = (reverseOfNumber * 10) + lastDigit ;
+    lastDigit = duplicateInput % 10;
+    console.log(lastDigit);
+
+    duplicateInput = duplicateInput - lastDigit ; 
+    duplicateInput = duplicateInput / 10 ;
+    reverseOfNumber = (reverseOfNumber * 10) + lastDigit ;
+
+}
 
 
+if(duplicateInput){
 
-lastDigit = duplicateInput % 10;
-console.log(lastDigit);
+    lastDigit = duplicateInput % 10;
+    console.log(lastDigit);
 
-duplicateInput = duplicateInput - lastDigit ; 
-duplicateInput = duplicateInput / 10 ;
-reverseOfNumber = (reverseOfNumber * 10) + lastDigit ;
+    duplicateInput = duplicateInput - lastDigit ; 
+    duplicateInput = duplicateInput / 10 ;
+    reverseOfNumber = (reverseOfNumber * 10) + lastDigit ;
+
+}
+
+if(duplicateInput){
+
+    lastDigit = duplicateInput % 10;
+    console.log(lastDigit);
+
+    duplicateInput = duplicateInput - lastDigit ; 
+    duplicateInput = duplicateInput / 10 ;
+    reverseOfNumber = (reverseOfNumber * 10) + lastDigit ;
+
+}
 
 
 console.log(reverseOfNumber);
