cf7b8bc3c0ebd31838cccec94f12dbea1cb91195
Gopi vinay <139052698+fake-smiler-115@users.noreply.github.com>
2025-09-20 13:18:32 +0530

    Replaced if-statements with a while loop

--- palindrome-program/number_to_reverse_number.js
+++ palindrome-program/number_to_reverse_number.js
@@ -1,49 +1,13 @@
-const input = 345;
+const input = 876;
 let lastDigit ; 
 
 let duplicateInput = input ;
 
 let reverseOfNumber = 0;
-if(duplicateInput){
+
+while(duplicateInput){
 
     lastDigit = duplicateInput % 10;
-    console.log(lastDigit);
-
-    duplicateInput = duplicateInput - lastDigit ; 
-    duplicateInput = duplicateInput / 10 ;
-    reverseOfNumber = (reverseOfNumber * 10) + lastDigit ;
-
-}
-
-
-if(duplicateInput){
-
-    lastDigit = duplicateInput % 10;
-    console.log(lastDigit);
-
-    duplicateInput = duplicateInput - lastDigit ; 
-    duplicateInput = duplicateInput / 10 ;
-    reverseOfNumber = (reverseOfNumber * 10) + lastDigit ;
-
-}
-
-
-if(duplicateInput){
-
-    lastDigit = duplicateInput % 10;
-    console.log(lastDigit);
-
-    duplicateInput = duplicateInput - lastDigit ; 
-    duplicateInput = duplicateInput / 10 ;
-    reverseOfNumber = (reverseOfNumber * 10) + lastDigit ;
-
-}
-
-if(duplicateInput){
-
-    lastDigit = duplicateInput % 10;
-    console.log(lastDigit);
-
     duplicateInput = duplicateInput - lastDigit ; 
     duplicateInput = duplicateInput / 10 ;
     reverseOfNumber = (reverseOfNumber * 10) + lastDigit ;
