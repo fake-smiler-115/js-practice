c7b0a927c5adc0cfe3943f74079460433be97afa
Gopi vinay <139052698+fake-smiler-115@users.noreply.github.com>
2025-09-20 12:32:31 +0530

    Implementing number reverse by extracting the last digit and dividing by 10

--- palindrome-program/number_to_reverse_number.js
+++ palindrome-program/number_to_reverse_number.js
@@ -1,6 +1,16 @@
-const input = 145;
+const input = 345;
 let lastDigit ; 
 
 lastDigit = input % 10;
  
 console.log(lastDigit);
+
+let duplicateInput = input ;
+
+duplicateInput = duplicateInput - lastDigit ;  // else getting floating values in dupicateInput
+
+duplicateInput = duplicateInput / 10 ;
+
+const reverseOfNumber = lastDigit ;
+
+console.log(duplicateInput);
