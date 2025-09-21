eae2a36897d565638f25cc2736d4d9545d52efaf
Gopi vinay <139052698+fake-smiler-115@users.noreply.github.com>
2025-09-21 23:37:59 +0530

    Added if statements to check the value of the decimal number

--- longest-one's-block-in-binary-number/decimal_number_to_binary_number.js
+++ longest-one's-block-in-binary-number/decimal_number_to_binary_number.js
@@ -1,36 +1,65 @@
-let decimalNumber = 13;
+let decimalNumber = 31;
 
 let binaryDigit ;
 let binaryNumber =0 ;
+let multiplingWithBinaryNumber = 1;
 
-binaryDigit = decimalNumber % 2;  
-console.log(binaryDigit);
-binaryNumber = binaryNumber  + (binaryDigit * 1) ; 
-decimalNumber = decimalNumber - binaryDigit ;
-decimalNumber = decimalNumber / 2;
+if(decimalNumber){
+    
+    binaryDigit = decimalNumber % 2;  
+    console.log(binaryDigit);
+    binaryNumber = binaryNumber  + (binaryDigit * multiplingWithBinaryNumber) ; 
+    decimalNumber = decimalNumber - binaryDigit ;
+    decimalNumber = decimalNumber / 2;
+    multiplingWithBinaryNumber *= 10;
 
-
-
-binaryDigit = decimalNumber % 2;  
-console.log(binaryDigit);
-binaryNumber = binaryNumber  + (binaryDigit * 10) ; 
-decimalNumber = decimalNumber - binaryDigit ;
-decimalNumber = decimalNumber / 2;
+}
 
 
+if(decimalNumber){
+    
+    binaryDigit = decimalNumber % 2;  
+    console.log(binaryDigit);
+    binaryNumber = binaryNumber  + (binaryDigit * multiplingWithBinaryNumber) ; 
+    decimalNumber = decimalNumber - binaryDigit ;
+    decimalNumber = decimalNumber / 2;
+    multiplingWithBinaryNumber *= 10;
 
-binaryDigit = decimalNumber % 2;  
-console.log(binaryDigit);
-binaryNumber = binaryNumber  + (binaryDigit * 100) ; 
-decimalNumber = decimalNumber - binaryDigit ;
-decimalNumber = decimalNumber / 2;
+}
 
 
-binaryDigit = decimalNumber % 2;  
-console.log(binaryDigit);
-binaryNumber = binaryNumber  + (binaryDigit * 1000) ; 
-decimalNumber = decimalNumber - binaryDigit ;
-decimalNumber = decimalNumber / 2;
+if(decimalNumber){
+    
+    binaryDigit = decimalNumber % 2;  
+    console.log(binaryDigit);
+    binaryNumber = binaryNumber  + (binaryDigit * multiplingWithBinaryNumber) ; 
+    decimalNumber = decimalNumber - binaryDigit ;
+    decimalNumber = decimalNumber / 2;
+    multiplingWithBinaryNumber *= 10;
+
+}
+
+if(decimalNumber){
+    
+    binaryDigit = decimalNumber % 2;  
+    console.log(binaryDigit);
+    binaryNumber = binaryNumber  + (binaryDigit * multiplingWithBinaryNumber) ; 
+    decimalNumber = decimalNumber - binaryDigit ;
+    decimalNumber = decimalNumber / 2;
+    multiplingWithBinaryNumber *= 10;
+
+}
+
+if(decimalNumber){
+    
+    binaryDigit = decimalNumber % 2;  
+    console.log(binaryDigit);
+    binaryNumber = binaryNumber  + (binaryDigit * multiplingWithBinaryNumber) ; 
+    decimalNumber = decimalNumber - binaryDigit ;
+    decimalNumber = decimalNumber / 2;
+    multiplingWithBinaryNumber *= 10;
+
+}
 
 
 console.log(binaryNumber);
