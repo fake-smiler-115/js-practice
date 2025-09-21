6de4fbf54902b53363148a29802ff44d9d97a532
Gopi vinay <139052698+fake-smiler-115@users.noreply.github.com>
2025-09-21 23:28:41 +0530

    Adding the binary digits to form the final binary number.

--- longest-one's-block-in-binary-number/decimal_number_to_binary_number.js
+++ longest-one's-block-in-binary-number/decimal_number_to_binary_number.js
@@ -1,16 +1,38 @@
 let decimalNumber = 13;
 
 let binaryDigit ;
+let binaryNumber =0 ;
 
-binaryDigit = decimalNumber % 2;
-
-// binary value of 13 = 1101
-
+binaryDigit = decimalNumber % 2;  
 console.log(binaryDigit);
-
+binaryNumber = binaryNumber  + (binaryDigit * 1) ; 
 decimalNumber = decimalNumber - binaryDigit ;
- 
 decimalNumber = decimalNumber / 2;
 
-console.log(decimalNumber);
+
+
+binaryDigit = decimalNumber % 2;  
+console.log(binaryDigit);
+binaryNumber = binaryNumber  + (binaryDigit * 10) ; 
+decimalNumber = decimalNumber - binaryDigit ;
+decimalNumber = decimalNumber / 2;
+
+
+
+binaryDigit = decimalNumber % 2;  
+console.log(binaryDigit);
+binaryNumber = binaryNumber  + (binaryDigit * 100) ; 
+decimalNumber = decimalNumber - binaryDigit ;
+decimalNumber = decimalNumber / 2;
+
+
+binaryDigit = decimalNumber % 2;  
+console.log(binaryDigit);
+binaryNumber = binaryNumber  + (binaryDigit * 1000) ; 
+decimalNumber = decimalNumber - binaryDigit ;
+decimalNumber = decimalNumber / 2;
+
+
+console.log(binaryNumber);
+
 
