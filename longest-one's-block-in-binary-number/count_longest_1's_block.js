bf8283a716cda81463ac0616648b0e189e9a47a9
Gopi vinay <139052698+fake-smiler-115@users.noreply.github.com>
2025-09-22 09:45:47 +0530

    Converted the number to a string and checked each binary digit

---
+++ longest-one's-block-in-binary-number/count_longest_1's_block.js
@@ -0,0 +1,20 @@
+let binaryNumber = 11101;
+
+binaryNumber = binaryNumber + " ";
+
+let trueOrFalse = (binaryNumber[0] === '1')  ;
+console.log(trueOrFalse);
+
+trueOrFalse = (binaryNumber[1] === '1')  ;
+console.log(trueOrFalse);
+
+trueOrFalse = (binaryNumber[2] === '1')  ;
+console.log(trueOrFalse);
+
+trueOrFalse = (binaryNumber[3] === '1')  ;
+console.log(trueOrFalse);
+
+trueOrFalse = (binaryNumber[4] === '1')  ;
+console.log(trueOrFalse);
+
+
