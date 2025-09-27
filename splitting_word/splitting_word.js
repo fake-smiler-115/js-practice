a83732cc144e98c758182ed9937e0079f15d19d6
Gopi vinay <139052698+fake-smiler-115@users.noreply.github.com>
2025-09-27 15:30:33 +0530

    seperating vowels, consonants in the word

--- splitting_word/splitting_word.js
+++ splitting_word/splitting_word.js
@@ -1,4 +1,9 @@
 function splitting(word, expectedOutput) {
+  const vowels = vowelsInWord(word);
+  const consonants = consonantsInWord(word);
+  console.log(vowels);
+  console.log(consonants);
+  
   const actualOutput = meregWord(word);
   const feedback = actualOutput === expectedOutput ? '👍' : '👎';
   printMessage(word, expectedOutput, actualOutput, feedback);
@@ -15,8 +20,51 @@ function meregWord(word) {
   return 'ape,p,l';
 }
 
+function checkLength(string) {
+  return string.length;
+}
+
+function vowelsInWord(word) {
+  let vowels = '';
+  
+  for (let index = 0; index < checkLength(word); index++) {
+
+    if (isVowel(word, index) ) {
+      vowels = vowels + word[index];
+    }
+  }
+
+  return vowels;
+}
+
+function consonantsInWord(word) {
+  let consonants = '';
+  
+  for (let index = 0; index < checkLength(word); index++) {
+
+    if (!isVowel(word, index) ) {
+      consonants = consonants + word[index];
+    }
+  }
+
+  return consonants;
+}
+
+function isVowel(word, index) {
+
+  if(word[index] === 'a' || word[index] === 'e' || word[index] === 'i') {
+    return true;
+  }
+
+  if(word[index] === 'o' || word[index] === 'u') {
+    return true;
+  }
+
+  return false;
+}
+
 splitting('apple', 'ape,p,l');
-// splitting('there', 'tere,h');
+splitting('there', 'tere,h');
 // splitting('hello', 'helo,l');
 // splitting('abyss', 'ab,y,s,s');
 // splitting('this', 'tis,h');
